0bccfe92fcc466db2afb147e162f716c67c5b726
devmonteiroux <95318216+devmonteiroux@users.noreply.github.com>
2022-02-03 01:05:28 -0300

    Add files via upload

---
+++ precache-manifest.2659a42dfe6f01e72b3ea50c1378eba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2757f643d76deae68ba09f3420d8292b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "654609af169df586a81e",
+    "url": "/static/css/main.2d61a50f.chunk.css"
+  },
+  {
+    "revision": "f45d18306bde70cb1e50",
+    "url": "/static/js/2.38c2d87d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.38c2d87d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "654609af169df586a81e",
+    "url": "/static/js/main.827c4c6b.chunk.js"
+  },
+  {
+    "revision": "a9deb5e7ee1ca92093a7",
+    "url": "/static/js/runtime-main.e721ff68.js"
+  }
+]);
